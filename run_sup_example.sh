f60109e0ba38d61be3733eb3de5470a7ba5353f8
emrecncelik <emrecncelik@gmail.com>
2023-01-24 22:02:39 +0300

    remove eval for now, edit script arguments

--- run_sup_example.sh
+++ run_sup_example.sh
@@ -14,10 +14,10 @@ export OMP_NUM_THREADS=8
 
 # Use distributed data parallel
 # If you only want to use one card, uncomment the following line and comment the line with "torch.distributed.launch"
-# python train.py \
-python -m torch.distributed.launch --nproc_per_node $NUM_GPU --master_port $PORT_ID train.py \
-    --model_name_or_path bert-base-uncased \
-    --train_file data/nli_for_simcse.csv \
+# python -m torch.distributed.launch --nproc_per_node $NUM_GPU --master_port $PORT_ID train.py \
+python train.py \
+    --model_name_or_path "dbmdz/bert-base-turkish-128k-uncased" \
+    --train_file data/nli_tr_for_simcse_hn.csv \
     --output_dir result/my-sup-simcse-bert-base-uncased \
     --num_train_epochs 3 \
     --per_device_train_batch_size 128 \
@@ -31,6 +31,8 @@ python -m torch.distributed.launch --nproc_per_node $NUM_GPU --master_port $PORT
     --overwrite_output_dir \
     --temp 0.05 \
     --do_train \
-    --do_eval \
     --fp16 \
     "$@"
+#    --do_eval \
+
+
